207e70ca1c4e15da4e60dbf15343c29d8effeccd
Nicolas Vasilache <nicolas.vasilache@gmail.com>
2017-03-18 00:10:11 +0000

    Isend -> Issend and remove useless barriers

--- scripts/ompirun.sh
+++ scripts/ompirun.sh
@@ -4,6 +4,6 @@ set -e
 
 # ./scripts/ompirun.sh -n 4 -npernode 4 -hostfile ${HOSTFILE} ./scripts/wrap.sh ${LUAJIT} ./test/collectives_all.lua -storage inplace -execution sync -processor gpu -tests p2p -benchmark -hierarchical false -staged false -numBuffers 2
 
-OMPI_OPTIONS=${OMPI_OPTIONS:='--mca btl_smcuda_cuda_max_send_size 10000000 --mca btl_smcuda_rndv_eager_limit 10000000 --mca btl_smcuda_eager_limit 10000000 --mca btl_smcuda_max_send_size 10000000 --mca btl_openib_max_send_size 10000000 --bind-to none'}
+OMPI_OPTIONS=${OMPI_OPTIONS:='--mca btl_smcuda_cuda_max_send_size 1000000 --mca btl_smcuda_max_send_size 1000000 --mca btl_openib_max_send_size 1000000 --bind-to none'}
 
 mpirun ${OMPI_OPTIONS} $@
